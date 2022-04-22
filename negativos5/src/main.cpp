724f1eef3f695cb24eff3b2cbebd6bd22ce9358d
Arthur87 <>
2022-04-22 21:34:44 +0000

    .

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -1,12 +1,33 @@
+//PASSOU NOS TESTES
+
 #include <iostream>
+
 using std::cin;
 using std::cout;
 
-const int SIZE = 5; // input size.
+const int SIZE = 5;
+const int ZERO = 0;
+const int ADC= 1;
 
-int main(void)
+int main(){
 {
-    // TODO: Adicione aqui seu código.
+    int x = 0;
+    int y; 
+    int saida=0;
 
+    while (x < SIZE)
+    {
+        cin >> std:ws y;
+        if (y < ZERO)
+        {
+            saida = ADC + saida;
+        }
+        x = x+ADC;
+    }
+    
+    cout << saida << endl;
+
+    
     return 0;
+
 }
