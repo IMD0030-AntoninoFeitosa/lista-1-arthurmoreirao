8c3349dd3c9053da66031cf1afc8045e059dcce6
Arthur87 <71678193+arthurmoreirao@users.noreply.github.com>
2022-04-22 21:40:37 +0000

    passou

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -1,21 +1,38 @@
-/*!
- * @brief This code implements the Intervalos programming problem
- * @author selan
- * @data June, 6th 2021
- */
+//PASSOU NOS TESTES
 
 #include <iostream>
+
 using std::cout;
 using std::cin;
 using std::endl;
 #include <iomanip>
 using std::setprecision;
 
-// Se desejar, crie funções aqui, antes do main().
+int main(){
 
-int main(void)
-{
-    // TODO: Adicione aqui a sua solução.
+    int range1, range2, range3, range4, range5;
+    int x, i;
+    range1 = range2 = range3 = range4 = range5 = 0;
 
+
+    while (cin >> std::ws >> x)
+    {
+	
+        if (x > 0 && x < 25){range1++;}
+        else if(x > 25 && x < 50){range2++;}
+        else if(x > 50 && x < 75){range3++;}
+        else if(x > 75 && x < 100){range4++;}
+        else{range5++;}
+	i++;
+    }
+
+
+
+    cout << setprecision(4) << (range1*100.0)/static_cast<double>(i) <<endl;
+  cout << setprecision(4) << (range2*100.0)/(i) <<endl;
+  cout << setprecision(4) << (range3*100.0)/(i) <<endl;
+  cout << setprecision(4) << (range4*100.0)/(i) <<endl;
+   cout << setprecision(4) << (range5*100.0)/(i) <<endl;
     return 0;
+
 }
